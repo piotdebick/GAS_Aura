b7b51f3805aceb1f36f114be14f6ca5e7baf82a1
piotdebick <piotrdebicki92@gmail.com>
2024-01-15 21:08:14 -0500

    combat interface

--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -19,6 +19,10 @@ public:
 	virtual void PossessedBy(AController* NewController) override;
 	virtual void OnRep_PlayerState() override;
 
+	/** Combat Interface */
+	virtual int32 GetPlayerLevel();
+	/** end Combat Interface*/
+
 private:
 	virtual void InitAbilityActorInfo() override;
 };
