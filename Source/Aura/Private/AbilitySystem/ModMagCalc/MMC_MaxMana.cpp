8503c69a43a63a3578db1c653d2c38dc050a0576
piotdebick <piotrdebicki92@gmail.com>
2024-01-17 15:05:01 -0500

    modmagcalc & set vital attributes

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
@@ -0,0 +1,33 @@
+// Copyright Piotr Debicki
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include <Interaction/CombatInterface.h>
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntelligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntelligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	IntelligenceDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(IntelligenceDef);
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	// Gather tags from source and target
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	EvaluationParameters.TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	float Intelligence = 0.f;
+	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, EvaluationParameters, Intelligence);
+	Intelligence = FMath::Max<float>(Intelligence, 0.f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return 50.f + 2.5f * Intelligence + 15.f * PlayerLevel;
+
+}
